5c61e236a7befbf616bcfafa417f7054cfa89dbb
honeykhra <honeykhra@gmail.com>
2023-02-10 21:26:23 +0530

    added react slick slider and made changes to home

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <>
-      <div className="divider"></div>
+      <div className="divider-footer"></div>
       <div className="footer">
         <div className="section1">
           <div className="logo">
