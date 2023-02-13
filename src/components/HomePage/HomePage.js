339ebb2a174583957e19a58cbd0f777e564f6c51
honeykhra <honeykhra@gmail.com>
2023-02-13 21:59:52 +0530

    some part added for category page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,18 +1,21 @@
 import React from "react";
 import "./Home.css";
 import LocationSearch from "../Search/LocationSearch/LocationSearch";
-import TopPicks from "./TopPicks";
-import Pg from "./Pg";
-import Flats from "./Flats";
+
+import Slider from "../SlickSlider/Slider";
+import Slider2 from "../SlickSlider/Slider2";
 
 const Home = () => {
   return (
     <div className="home">
       <LocationSearch />
       <div className="section">
-        <TopPicks />
-        <Pg />
-        <Flats />
+        <h4>TopPicks</h4>
+        <Slider />
+        <h4>Pg</h4>
+        <Slider2 />
+        <h4>Flats</h4>
+        <Slider2 />
       </div>
     </div>
   );
--- src/pages/category/Category.js
+++ src/pages/category/Category.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React from "react";
+import "./Category.css";
+import Footer from "../../components/Footer/Footer";
+import Navbar from "../../components/Navbar/Navbar";
+
+import Slider2 from "../../components/SlickSlider/Slider2";
 
 const Category = () => {
-    return (
-        <div>Category</div>
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <div className="content-container">
+        <div className="top-bar">
+          <h4 className="top-bar-h4">Home / PG</h4>
+          <input
+            className="top-bar-search"
+            type="search"
+            name="search"
+            id="search"
+            placeholder="Search Rooms"
+          />
+        </div>
+        <h4>Related Suggestions</h4>
+        <Slider2 />
+        <h4>Pg</h4>
+        <Slider2 />
+        <h4>Flats</h4>
+        <Slider2 />
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Category;
