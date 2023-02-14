561f3c25690e87f021f034912c27b00464ed5c50
honeykhra <honeykhra@gmail.com>
2023-02-14 21:52:56 +0530

    Added header footer to addroom

--- src/pages/Add/AddRoom.js
+++ src/pages/Add/AddRoom.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import "./AddRoom.css";
+
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
 
 const AddRoom = () => {
-    return (
-        <div>AddRoom</div>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <div className="add-room">AddRoom</div>
+      <Footer />
+    </>
+  );
+};
 
 export default AddRoom;
