a77b43bd0a43cb75291059ea8342d83a66a316e9
honeykhra <honeykhra@gmail.com>
2023-02-25 20:01:10 +0530

    none

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Col, Container, Form, InputGroup, Row, Image, Nav } from "react-bootstrap";
+import {
+  Col,
+  Container,
+  Form,
+  InputGroup,
+  Row,
+  Image,
+  Nav,
+} from "react-bootstrap";
 import OwnerDetails from "./OwnerDetails";
 import PropertyImages from "./PropertyImages";
 import locationIcon from "../../assets/images/location_icon.svg";
@@ -16,35 +24,38 @@ Design is a plan or specification for the construction of an object or system or
 const PropertyDetails = () => {
   return (
     <>
-    {/* Navbar */}
-    <NavBar/>
+      {/* Navbar */}
+      <NavBar />
 
       <Container fluid>
         <Container className="property-details-topbar-container">
-        <Row>
-          <Col className="mt-2 property-details-breadcrumb-item">Home / PG / Details</Col>
-          <Col>
-            <h1 style={{ color: "#152C5B" }}>Village Angara</h1>
-            <p className="property-location-details ml-3 mt-3">
-              <Image
-                src={locationIcon}
-                alt={"location"}
-                width={23}
-                height={25}
-              />
-              <span className="ml-3 location-text"> Chandigarh, India</span>
-            </p>
-          </Col>
-          <Col xs={2} className="property-details-search-input">
-            <InputGroup size="sm" className="mt-3">
-              <Form.Control
-                placeholder="Search Rooms......"
-                aria-label="Small"
-                aria-describedby="inputGroup-sizing-sm"
-              />
-            </InputGroup>
-          </Col>
-        </Row>
+          <Row>
+            <Col className="mt-2 property-details-breadcrumb-item">
+              Home / PG / Details
+            </Col>
+            <Col>
+              <h1 style={{ color: "#152C5B" }}>Village Angara</h1>
+              <p className="property-location-details ml-3 mt-3">
+                <Image
+                  src={locationIcon}
+                  alt={"location"}
+                  width={23}
+                  height={25}
+                />
+                <span className="ml-3 location-text"> Chandigarh, India</span>
+              </p>
+            </Col>
+            <Col xs={2} className="property-details-search-input">
+              <InputGroup size="sm" className="mt-3">
+                <Form.Control
+                  class="input"
+                  placeholder="Search Rooms......"
+                  aria-label="Small"
+                  aria-describedby="inputGroup-sizing-sm"
+                />
+              </InputGroup>
+            </Col>
+          </Row>
         </Container>
         {/* Property Images Section */}
         <PropertyImages />
