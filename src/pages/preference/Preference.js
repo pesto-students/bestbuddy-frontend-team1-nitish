1a7b65f14a17bd39f740c65cca769984a1fd62ad
honeykhra <honeykhra@gmail.com>
2023-02-25 18:53:46 +0530

    created preferencegrid as a reuseable component

--- src/pages/preference/Preference.js
+++ src/pages/preference/Preference.js
@@ -1,37 +1,12 @@
 import React from "react";
 import "./Preference.scss";
-
-import BookWorm from "../../assets/preferences/Book-worm.svg";
-import Fashionable from "../../assets/preferences/Fashionable.svg";
-import FitnessFreak from "../../assets/preferences/Fitness-Freak.svg";
-import MediaBuff from "../../assets/preferences/Media-Buff.svg";
-import Nondrinker from "../../assets/preferences/Non-Drinker.svg";
-import PartyAnimal from "../../assets/preferences/Party-Animal.svg";
-import Professional from "../../assets/preferences/Professional.svg";
-import Student from "../../assets/preferences/Student.svg";
-import Vegan from "../../assets/preferences/Vegan.svg";
-import MusicLover from "../../assets/preferences/Music-Lover.svg";
-import PetAllowed from "../../assets/preferences/Pet-Allowed.svg";
-import Religious from "../../assets/preferences/Religious.svg";
+import PreferenceGrid from "../../components/PreferenceGrid/PreferenceGrid";
 
 const Preference = () => {
   return (
-    <div className="Preference">
-      <h3> Preferences</h3>
-      <div className="grid">
-        <img src={BookWorm} alt="BookWorm" />
-        <img src={Fashionable} alt="Fashionable" />
-        <img src={FitnessFreak} alt="FitnessFreak" />
-        <img src={MediaBuff} alt="MediaBuff" />
-        <img src={Nondrinker} alt="Nondrinker" />
-        <img src={PartyAnimal} alt="PartyAnimal" />
-        <img src={Professional} alt="Professional" />
-        <img src={Student} alt="Student" />
-        <img src={Vegan} alt="Vegan" />
-        <img src={MusicLover} alt="MusicLover" />
-        <img src={PetAllowed} alt="PetAllowed" />
-        <img src={Religious} alt="Religious" />
-      </div>
+    <div>
+      <h3>Preference</h3>
+      <PreferenceGrid />
     </div>
   );
 };
