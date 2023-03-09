b5d3c9d8f02bb01b522e9a3ca771588533604ae1
honeykhra <honeykhra@gmail.com>
2023-03-09 07:38:18 -0800

    inputs ad

--- src/pages/Add/AddRoom.js
+++ src/pages/Add/AddRoom.js
@@ -15,6 +15,44 @@ const AddRoom = () => {
       <div className="add-room">
         <h2 className="addHeading">Have a Room</h2>
         <h6 className="addHeading2">Please enter the room details below</h6>
+
+        <div className="inputs-grpstop">
+          <div className="input-grp grp1">
+            <div className="input-location">
+              <h5>Add room location</h5>
+              <input type="text" name="location" id="" />
+            </div>
+            <div className="input-rent">
+              <h5>Rent of Room</h5>
+              <input
+                type="text"
+                name="rent"
+                id=""
+                placeholder="Please input here"
+              />
+            </div>
+          </div>
+          <span className="divider1"></span>
+          <div className="input-grp grp2">
+            <div className="input-number">
+              <h5>Phone Number</h5>
+              <input
+                type="text"
+                name="number"
+                id=""
+                placeholder="Please input here"
+              />
+            </div>
+            <div className="input-gender">
+              <h5>Gender of RoomMate</h5>
+              <div className="buttons">
+                <button>Male</button>
+                <button>FeMale</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <h2 className="addHeading">Select available Amenties</h2>
         <Amenties />
         <div className="imageupload">
@@ -27,12 +65,32 @@ const AddRoom = () => {
             max 10 images
           </p>
         </div>
+
+        <div className="input-grp grp3">
+          <div className="input-availability">
+            <h5>Room is available from</h5>
+            <input
+              type="date"
+              name="date"
+              id=""
+              placeholder="Please input here"
+            />
+          </div>
+
+          <span className="divider2"></span>
+
+          <div className="input-occupancy">
+            <h5>Current Occupancy of Room</h5>
+            <button>Single</button>
+            <button>Shared</button>
+          </div>
+        </div>
+
         <h2 className="addHeading">
           Preferences for Room Partner(select 3 atleast)
         </h2>
         <PreferenceGrid />
         <Button className="submitbutton">Submit</Button>
-        
       </div>
       <Footer />
     </>
