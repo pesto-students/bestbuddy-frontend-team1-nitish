cfcace203d0ac762e6ead26dc03842c944435384
honeykhra <honeykhra@gmail.com>
2023-03-16 18:49:31 +0530

    best buddy axios and store changes

---
+++ src/bestbuddyaxios/bestBuddyAxios.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const token = localStorage.getItem("access-token");
+
+const bestBuddyAxios = axios.create({
+  baseURL: "https://rich-lime-pig-tam.cyclic.app/",
+  headers: {
+    "access-token": token,
+  },
+});
+
+export default bestBuddyAxios;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import TopPicks from "../../components/Toppicks/TopPicks";
 import Slider from "../../components/shared/Slider/Slider";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
-import LocationSearch from './../../components/Search/LocationSearch/LocationSearch';
+import LocationSearch from "./../../components/Search/LocationSearch/LocationSearch";
+import { fetchAllProperties } from "../../store/slice/property/propertySlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  const properties = useSelector((state) => state.property.allProperties);
+
+  useEffect(() => {
+    dispatch(fetchAllProperties());
+  }, []);
+
+  // useEffect(() => {
+  //   console.log(properties);
+  // }, [properties]);
+
   return (
     <div>
       <Navbar />
-      <LocationSearch />
       <Link to={"/details/1"}>Details</Link>
       <div className="container">
         <TopPicks />
-        <Slider title="PG" />
-        <Slider title="Flats" />
+        <Slider properties={properties} />
+        {/* <Slider title="Flats" />
         <Slider title="Apartment" />
-        <Slider title="Residency" />
+        <Slider title="Residency" /> */}
       </div>
       <Footer />
     </div>
