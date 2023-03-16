3e0cce0a764d082828f2847be6be38f3f58cda0b
Durgesh2601 <dk829445@gmail.com>
2023-03-17 01:16:26 +0530

    sentry integration

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import "./index.scss";
 import App from "./App";
 import { Store } from "./store/Store";
 
-// Sentry.init({
-//   dsn: "https://8a876e70e31c4d4f81b67f30c3d86105@o4504844072714240.ingest.sentry.io/4504844114198528",
-//   integrations: [new Integrations.BrowserTracing()],
-//   tracesSampleRate: 1.0, //lower the value in production
-// });
+Sentry.init({
+  dsn: "https://8a876e70e31c4d4f81b67f30c3d86105@o4504844072714240.ingest.sentry.io/4504844114198528",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0, //lower the value in production
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
