cc27bed7c6f0ef0a951c3d4a0655873689e06560
Durgesh2601 <durgesh.singh@sourcewiz.co>
2023-03-29 01:49:21 +0530

    fix: api routes

--- src/bestbuddyaxios/bestBuddyAxios.js
+++ src/bestbuddyaxios/bestBuddyAxios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const token = localStorage.getItem("access-token");
 
 const bestBuddyAxios = axios.create({
-  // baseURL: "https://rich-lime-pig-tam.cyclic.app/",
-  baseURL: "http://localhost:8000/",
+  baseURL: "https://rich-lime-pig-tam.cyclic.app/",
+  // baseURL: "http://localhost:8000/",
   headers: {
     "access-token": token,
   },
