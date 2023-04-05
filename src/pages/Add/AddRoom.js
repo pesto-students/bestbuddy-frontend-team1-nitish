bf2ef0d98a1bd09128ecea085ea9094653081465
honeykhra <honeykhra@gmail.com>
2023-04-05 08:23:08 +0530

    add room style fix

--- src/pages/Add/AddRoom.js
+++ src/pages/Add/AddRoom.js
@@ -6,7 +6,7 @@ import NavBar from "../../components/Navbar/Navbar";
 import Amenties from "./../../components/Amenties/Amenties";
 import PreferenceGrid from "./../../components/PreferenceGrid/PreferenceGrid";
 import { Button } from "react-bootstrap";
-import { getFormValues } from "../../utils/formFieldHelpers";
+import { getFormValues, uploaderTexts } from "../../utils/formFieldHelpers";
 import "./AddRoom.scss";
 import {
   addProperty,
@@ -52,12 +52,12 @@ const AddRoom = () => {
   return (
     <>
       <NavBar />
-      <form className="container" id="property-form" onSubmit={(e) => handleSubmit(e)}>
+      <form id="property-form" onSubmit={(e) => handleSubmit(e)}>
         <div className="add-room">
           <h2 className="addHeading">Have a Room</h2>
           <h6 className="addHeading2">Please enter the room details below</h6>
 
-          <section className="inputs-grps-top">
+          <div className="inputs-grps-top">
             <div className="input-grp grp1">
               <div className="input-propertyname">
                 <h5>Property name</h5>
@@ -72,8 +72,12 @@ const AddRoom = () => {
               <div className="input-location">
                 <h5>Add room location</h5>
                 <select name="location" className="select">
-                  <option disabled value="">Select a location</option>
+                  <option disabled value="">
+                    Select a location
+                  </option>
                   <option value="Delhi">Delhi</option>
+                  <option value="Delhi">Chandigarh</option>
+                  <option value="Delhi">Bangalore</option>
                   <option value="Chennai">Chennai</option>
                   <option value="Mumbai">Mumbai</option>
                   <option value="Hyderabadh">Hyderabadh</option>
@@ -95,7 +99,6 @@ const AddRoom = () => {
               <div className="input-type">
                 <h5>Property type</h5>
                 <select name="property-type" className="select">
-                  <option disabled value="">Select a category</option>
                   <option value="Flat">Flat</option>
                   <option value="PG">PG</option>
                   <option value="Apartment">Apartment</option>
@@ -115,43 +118,40 @@ const AddRoom = () => {
               <div className="input-gender">
                 <h5>Gender of RoomMate</h5>
                 <select name="gender" className="select">
-                  <option disabled value="">Select a Gender</option>
                   <option value="male">Male</option>
                   <option value="female">Female</option>
                 </select>
               </div>
             </div>
-          </section>
+          </div>
 
           <h2 className="addHeading">Select available Amenties</h2>
           <Amenties selectable={true} setAmenties={setAmenties} />
 
           <ImageUploader setImages={setImages} />
 
-          <div className="inputs-grps-top">
-            <div className="input-grp grp1">
-              <div className="input-availability ">
-                <h5>Room is available from</h5>
-                <input
-                  type="date"
-                  name="date"
-                  id=""
-                  min={new Date().toISOString().split("T")[0]}
-                  placeholder="Please input here"
-                />
-              </div>
+          <div className="input-grp grp3">
+            <div className="input-availability">
+              <h5>Room is available from</h5>
+              <input
+                type="date"
+                name="date"
+                id=""
+                min={new Date().toISOString().split("T")[0]}
+                placeholder="Please input here"
+              />
             </div>
+
             <span className="divider2"></span>
-            <div className="input-grp grp2">
-              <div className="input-occupancy grp2">
-                <h5>Current Occupancy of Room</h5>
-                <div className="buttons">
-                  <select name="occupancy" className="select">
-                    <option value="single">Single</option>
-                    <option value="double">Shared(2)</option>
-                    <option value="tripple">Shared(3)</option>
-                  </select>
-                </div>
+
+            <div className="input-occupancy">
+              <h5>Current Occupancy of Room</h5>
+              <div className="buttons">
+                <select name="occupancy" className="select">
+                  <option value="single">Single</option>
+                  <option value="double">Shared(2)</option>
+                  <option value="tripple">Shared(3)</option>
+                </select>
               </div>
             </div>
           </div>
