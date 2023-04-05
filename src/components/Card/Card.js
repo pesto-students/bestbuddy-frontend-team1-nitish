79e0423b53840de25f3b1a9de32194a94825f5d5
honeykhra <honeykhra@gmail.com>
2023-04-05 08:37:00 +0530

    tmatch added toppicks card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,25 @@
 import React from "react";
-
 import "./Card.scss";
-// import image1 from "../../assets/images/kara-eads-L7EwHkq1B2s-unsplash.jpg";
+import Matchimg from "../../assets/Match.svg";
+import { Link } from "react-router-dom";
 
 const Card = ({ property }) => {
   return (
     <div className="card">
-      <div className="card-image">
-        <img src={property.image[0]} alt="cardimage" />
-      </div>
-      <div className="heading">
-        <h5>{property.name}</h5>
-        <h6>{property.city}</h6>
-      </div>
-      <div className="badge">Best Match</div>
+      <Link to={`/details/${property?._id}`} key={property?._id}>
+        <div className="card-image">
+          <img src={property.image[0]} alt="cardimage" />
+        </div>
+        <div className="heading">
+          <h5>{property.name}</h5>
+          <h6>{property.city}</h6>
+          <div className="matchimg">
+            <img src={Matchimg} alt="Match" />
+            <h6>90% Match</h6>
+          </div>
+        </div>
+        <div className="badge">Top Match</div>
+      </Link>
     </div>
   );
 };
