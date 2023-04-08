dc7b01411f96264c5a7e4ce7a40ef46a2be45ad8
mufasilmuf <mufasilmuf@gmail.com>
2023-04-08 22:39:46 +0530

    Skeleton Loading for Topicks and Filters

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,5 +1,9 @@
-import React,{ useState } from "react";
-import "./Filter.scss";
+import React from 'react';
+import { useState } from 'react';
+
+import './Filter.scss';
+import { useSelector } from 'react-redux';
+import FilterSkeleton from '../Skeleton/FilterSkeleton';
 
 const filterData = [
   {
@@ -21,8 +25,9 @@ const filterData = [
 ];
 
 const Filter = () => {
+  const isLoading = useSelector(state => state.property.isLoading);
   const [toggle, setToggle] = useState(false);
-  const [filterValue, setfilterValue] = useState({});
+  const [filterValue, setfilterValue] = useState({})
 
   const handleFilter = (e) => {
     setfilterValue((prev) => ({ ...prev, [e.target.name]: e.target.value }));
@@ -32,27 +37,27 @@ const Filter = () => {
   };
 
   return (
-    <div className="container filter-container">
-      <p onClick={() => setToggle(!toggle)}>
-        <span>Search Properties by Filter</span>
-        <span className={`arrow ${toggle ? "up" : "down"}`}></span>
-      </p>
-      <div className={`row filter-section ${toggle ? "show" : ""}`}>
-        {filterData.map((item) => (
-          <section className="col-sm-12 col-md-6 col-lg-3">
-            <select name={item.name} onChange={(e) => handleFilter(e)}>
-              <option disabled defaultValue={item.name}>
-                {item.name}
-              </option>
-              {item.value.map((val) => (
-                <option value={val}>{val}</option>
-              ))}
-            </select>
-          </section>
-        ))}
-      </div>
+    <div className='container filter-container'>
+      {isLoading ? <FilterSkeleton /> : <>
+        <p onClick={() => setToggle(!toggle)}>
+          <span>Search Properties by Filter</span>
+          <span className={`arrow ${toggle ? 'up' : 'down'}`}></span>
+        </p>
+        <div className={`row filter-section ${toggle ? 'show' : ''}`}>
+          {filterData.map((item) => (
+            <section className='col-sm-12 col-md-6 col-lg-3' key={item.name}>
+              <select name={item.name} onChange={(e) => handleFilter(e)}>
+                <option value="none" selected disabled hidden>Select a {item.name}</option>
+                {item.value.map((val) => (
+                  <option key={val} value={val}>{val}</option>
+                ))}
+              </select>
+            </section>
+          ))}
+        </div>
+      </>}
     </div>
-  );
-};
+  )
+}
 
 export default Filter;
--- src/components/Toppicks/TopPicks.js
+++ src/components/Toppicks/TopPicks.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import "./TopPicks.scss";
 import Card from "../Card/Card";
+import TopPickSkeleton from "../Skeleton/TopPickSkeleton";
+
 
 const TopPicks = ({ properties }) => {
+  const isLoading = useSelector((state) => state.property.isLoading)
   return (
-    <div className="toppicks-container">
-      {properties.map((property) => (
-        <Card property={property} key={property._id} />
-      ))}
-    </div>
+    <>
+      {isLoading ? <TopPickSkeleton /> : <div className="toppicks-container">
+        {properties.map((property) => (
+          <Card property={property} key={property._id} />
+        ))}
+      </div>}
+
+    </>
   );
 };
 
