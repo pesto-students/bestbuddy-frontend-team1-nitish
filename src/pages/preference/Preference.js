5011e46cb0b2710586941e6a3ee89186a0913cc1
honeykhra <honeykhra@gmail.com>
2023-04-11 21:26:03 +0530

    now user can select preferences from frontend

--- src/pages/preference/Preference.js
+++ src/pages/preference/Preference.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { editUserDetails } from "../../store/slice/users/userSlice";
 import "./Preference.scss";
 
 import Footer from "../../components/Footer/Footer";
@@ -7,13 +9,34 @@ import PreferenceGrid from "../../components/PreferenceGrid/PreferenceGrid";
 import { Button } from "react-bootstrap";
 
 const Preference = () => {
+  const [preferences, setPreferences] = useState([]);
+  const dispatch = useDispatch();
+  const userInfo = useSelector((state) => state?.user?.userInfo);
+  const id = userInfo?.id;
+
+  const handleSubmit = (e) => {
+    e?.preventDefault();
+    if (preferences?.length < 3) {
+      return alert(`Please select atleast 3 preferences`);
+    }
+    const data = { preferences: preferences };
+
+    const payload = {
+      data,
+      id,
+    };
+    dispatch(editUserDetails(payload));
+  };
+
   return (
     <div>
       <Navbar />
       <div className="container preferences-container">
         <h3 className="preferences-heading">Preferences</h3>
-        <PreferenceGrid />
-        <Button className="submitbutton">Continue</Button>
+        <PreferenceGrid selectable={true} setPreferences={setPreferences} />
+        <Button className="submitbutton" onClick={() => handleSubmit()}>
+          Continue
+        </Button>
       </div>
       <Footer />
     </div>
