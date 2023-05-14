a2f0d505a362431948dcf7b9ad210a6629139cfb
Durgesh2601 <dk829445@gmail.com>
2023-05-14 20:46:23 +0530

    fix property details skeleton

--- src/components/Skeleton/PropertyDetailsSkeleton.js
+++ src/components/Skeleton/PropertyDetailsSkeleton.js
@@ -4,11 +4,9 @@ const PropertyDetailsSkeleton = () => {
   return (
     <div className="skeleton-container-property">
       <Skeleton height={70} borderRadius={10} />
+      <Skeleton height={550} borderRadius={10} />
       <Skeleton height={70} borderRadius={10} />
-      <Skeleton height={70} borderRadius={10} />
-      <Skeleton height={70} borderRadius={10} />
-      <Skeleton height={70} borderRadius={10} />
-      <Skeleton height={70} borderRadius={10} />
+      <Skeleton height={450} borderRadius={10} />
     </div>
   );
 };
