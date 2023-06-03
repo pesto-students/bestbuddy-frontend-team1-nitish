423929ec36a8c1575c045f9621f0f0269714a185
Durgesh2601 <dk829445@gmail.com>
2023-06-03 14:41:41 +0530

    image on load check

--- src/components/Card/SliderCard.js
+++ src/components/Card/SliderCard.js
@@ -3,23 +3,31 @@ import "./SliderCard.scss";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import SliderCardSkeleton from "../Skeleton/SliderCardSkeleton";
+import ImageWrapper from "../ImageWrapper";
 
 const SliderCard = ({ property }) => {
-  const isLoading = useSelector(state => state.property.isLoading);
+  const isLoading = useSelector((state) => state.property.isLoading);
 
   return (
     <>
-      {isLoading ? <SliderCardSkeleton /> : <Link to={`/details/${property?._id}`} key={property?._id}>
-        <div className="card2">
-          <div className="card-image">
-            <img loading="lazy" src={property?.image[0]} alt="cardimage" />
+      {isLoading ? (
+        <SliderCardSkeleton />
+      ) : (
+        <Link to={`/details/${property?._id}`} key={property?._id}>
+          <div className="card2">
+            <div className="card-image">
+              <ImageWrapper
+                imgSrc={property?.image?.[0]}
+                style={{ height: "200px" }}
+              />
+            </div>
+            <div className="card2-heading">
+              <h5>{property?.name}</h5>
+              <h6>{property?.city}</h6>
+            </div>
           </div>
-          <div className="card2-heading">
-            <h5>{property?.name}</h5>
-            <h6>{property?.city}</h6>
-          </div>
-        </div>
-      </Link>}
+        </Link>
+      )}
     </>
   );
 };
