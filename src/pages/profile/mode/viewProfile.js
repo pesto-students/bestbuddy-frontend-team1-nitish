8ceb46c04bc80937ac1539aca65685779553fc22
Durgesh2601 <dk829445@gmail.com>
2023-06-10 22:50:55 +0530

    profile pic render in view mode

--- src/pages/profile/mode/viewProfile.js
+++ src/pages/profile/mode/viewProfile.js
@@ -1,39 +1,57 @@
-import { Button } from "react-bootstrap";
+import { Button, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { capitalizeFirstLetter } from "../../../utils/formFieldHelpers";
+import ImageWrapper from "../../../components/ImageWrapper";
+import { fieldRenderer } from "../../../utils/formFieldHelpers";
 import "./viewProfile.scss";
 
+const stylesObj = {
+  width: "110px",
+  height: "105px",
+  borderRadius: "50%",
+};
+
+const imgStyle = {
+  width: "130px",
+  height: "105px",
+  borderRadius: "50%",
+};
+
 export const ViewProfile = () => {
-  const userInfo = useSelector((state) => state?.user?.userInfo);
+  const { userInfo, isLoading } = useSelector((state) => state?.user);
 
   return (
-    <>
-      <div className="viewProfile">
-        <div className="field name">
-          <h5>Name</h5>
-          <p>{userInfo?.name}</p>
-        </div>
-        <div className="field email">
-          <h5>Email Address</h5>
-          <p>{userInfo?.email}</p>
-        </div>
-        <div className="field city">
-          <h5>City</h5>
-          <p>{userInfo?.city}</p>
-        </div>
-        <div className="field number">
-          <h5>Phone Number</h5>
-          <p>{userInfo?.number}</p>
-        </div>
-        <div className="field gender">
-          <h5>Gender</h5>
-          <p>{capitalizeFirstLetter(userInfo?.gender) || userInfo?.gender}</p>
-        </div>
-        <Link to={"/profile/edit"}>
-          <Button className="editbutton">Edit details</Button>
-        </Link>
+    <div className="viewProfile">
+      <Row align="center" className="view-profile-pic-row">
+        <ImageWrapper
+          imgSrc={userInfo?.profile_pic}
+          style={stylesObj}
+          imgStyle={imgStyle}
+        />
+      </Row>
+      <div className="field name">
+        <h5>Name</h5>
+        <p>{fieldRenderer(isLoading, "name", userInfo)}</p>
       </div>
-    </>
+      <div className="field email">
+        <h5>Email Address</h5>
+        <p>{fieldRenderer(isLoading, "email", userInfo)}</p>
+      </div>
+      <div className="field city">
+        <h5>City</h5>
+        <p>{fieldRenderer(isLoading, "city", userInfo)}</p>
+      </div>
+      <div className="field number">
+        <h5>Phone Number</h5>
+        <p>{fieldRenderer(isLoading, "number", userInfo)}</p>
+      </div>
+      <div className="field gender">
+        <h5>Gender</h5>
+        <p>{fieldRenderer(isLoading, "gender", userInfo)}</p>
+      </div>
+      <Link to={"/profile/edit"}>
+        <Button className="editbutton">Edit details</Button>
+      </Link>
+    </div>
   );
 };
