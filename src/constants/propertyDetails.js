291320466908f224c14ee483658e97abaeaa2ce6
Durgesh2601 <dk829445@gmail.com>
2023-06-15 00:23:52 +0530

    fix add key in constants

--- src/constants/propertyDetails.js
+++ src/constants/propertyDetails.js
@@ -2,7 +2,7 @@ const moreDetailsPara = `
 Design is a plan or specification for the construction of an object or system or for the implementation of an activity or process, or the result of that plan or specification in the form of a prototype, product or process. The national agency for design: enabling Singapore to use design for economic growth and to make lives better. Design is a plan or specification for the construction of an object or system or for the implementation of an activity or process, or the result of that plan or specification in the form of a prototype, product or process. The national agency for design: enabling Singapore to use design for economic growth and to make lives better.`;
 
 const PropertyHeaders = {
-  view: {
+  add: {
     title: "Have a Room",
     subtitle: "Please enter the room details below",
   },
